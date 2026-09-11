7443d566eb580d5ad8cfd8cfd3964736329d4f31
MahediHasan03 <mahedihasanapon333@gmail.com>
2026-09-12 01:51:27 +0600

    Controlled form

--- src/Components/ControlledField/ControlledField.jsx
+++ src/Components/ControlledField/ControlledField.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const ControlledField = () => {
+
+    const [password, setPassword] = useState('');
     
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(e.target.password.value)
+        console.log('submitted')
+    }
+
+    const handlePasswordOnChange = e => {
+        console.log(e.target.value)
     }
     
     return (
@@ -12,7 +18,7 @@ const ControlledField = () => {
             <form onSubmit={handleSubmit}>
                 <input type="text" name='name' placeholder='Your Name'  />
                 <br />
-                <input type="password" name='password'  placeholder='Password' required/>
+               <input type="password" name='password' onChange={handlePasswordOnChange}  defaultValue={password}  placeholder='Password' required/>
                 <br />
                 <input type="submit" />
             </form>
